53ec8faef5976b06a01cbd5ea42a289e9dd988b3
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2023-08-14 16:08:49 +0530

    Add files via upload

---
+++ CH1.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+ clrscr();
+
+ printf("========== Resume ==========\n\n");
+
+ printf("  First name\t: Parth\n\n");
+
+ printf("  Last name\t: Bhatiya\n\n");
+
+ printf("  Email id\t: Parth.bhatiya3618@gmail.com\n\n");
+
+ printf("  Mobile number\t\: +91 7096250691\n\n");
+
+ printf("  Study\t\t: C Laguage\n\n");
+
+ printf("  City\t\t: Surat\n\n");
+
+ printf("  County\t: India\n\n");
+
+ printf("  Age\t\t: 20\n\n");
+
+ printf("===== Thank you !! =====");
+
+
+
+
+
+ getch();
+}
